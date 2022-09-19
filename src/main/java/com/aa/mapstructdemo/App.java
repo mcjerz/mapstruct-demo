979a5b17767db578f90f1bc5301a92906130b275
alyson albasi <aly@mcjerz.net>
2022-09-19 12:22:45 -0400

    mapstruct spring boot demo starter

--- src/main/java/com/aa/mapstructdemo/App.java
+++ src/main/java/com/aa/mapstructdemo/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MapstructDemoApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(MapstructDemoApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
